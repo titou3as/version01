a11acd19c15ae1fc9d7f1018ed0237d3a099c8d0
khaoula-abaidi <abaidik@gmail.com>
2019-04-15 14:46:50 +0200

    Changements

--- src/Controller/ContributorController.php
+++ src/Controller/ContributorController.php
@@ -20,7 +20,8 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class ContributorController extends AbstractController
 {
     /**
-     * @Route("/index", name="contributor_index",methods={"GET","POST"})
+     * @Route("/index", name="contributor_index")
+     * @var Request $request
      */
     public function index(ContributorRepository $repository, ObjectManager $manager,DecisionRepository $decisionRepository, Request $request): Response
     {
@@ -37,34 +38,50 @@ class ContributorController extends AbstractController
          * @var Decision[] $decisions
          */
         $decisions = $contributor->getDecisions()->filter(function ($decision){
+            /** @var Decision $decision */
             return $decision->getIsTaken() == false;
         });
         $contributor->setDecisionsNT($decisions);
         $form = $this->createForm(ContributorType::class,$contributor);
         $form->handleRequest($request);
-        if($form->isSubmitted()){
+        if($form->isSubmitted() && $form->isValid()){
             /**
              * Update each decision with new  Form data
              */
-            foreach ($decisions as $decision){
+
+            foreach ($contributor->getDecisionsNT() as $decision) {
                 /**
-                 * Access Roles Validation
+                 * Access Roles Validation : If $contributor is the owner of $decision , He is allow to update it
                  */
-                $this->denyAccessUnlessGranted('update',$contributor);
-                switch ($decision->getDeposit()){
-                    case 'oui' : $decision->setIsTaken(true);$decision->setContent('Dépôt');break;
-                    case 'non' : $decision->setIsTaken(true);$decision->setContent('Refus Dépôt');break;
+                $this->isGranted('update', $contributor);
+
+                switch ($decision->getDeposit()) {
+                    case 'oui' :
+                        $decision->setIsTaken(true);
+                        $decision->setContent('Dépôt');
+                        break;
+                    case 'non' :
+                        $decision->setIsTaken(true);
+                        $decision->setContent('Refus Dépôt');
+                        break;
                     default    : //$decision->setIsTaken(null);
-                        $decision->setContent('En attente');break;
+                        $decision->setContent('En attente');
+                        break;
                 }
+
+
+
+            }
                 /**
                  * Saving the contributor's new state decisions
                  */
+
                 $manager->flush();
                 $this->addFlash('success','Les nouvelles décisions sont prises en compte');
                 return $this->redirectToRoute('contributor_index');
             }
-        }
+
+      // dump($decisions);die;
         return $this->render('contributor/index.html.twig', [
             'contributor' => $contributor,
             'decisions' => $decisions,

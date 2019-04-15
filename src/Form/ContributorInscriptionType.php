c6a28cf19934f192c8dfae60112cd3b71d03acff
khaoula-abaidi <abaidik@gmail.com>
2019-04-15 17:15:23 +0200

    Customize inscription

--- src/Form/ContributorInscriptionType.php
+++ src/Form/ContributorInscriptionType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Contributor;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,46 @@ class ContributorInscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('password')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('email')
+            ->add('username',TextType::class,[
+                                         'label' => 'Login: ',
+                                         'attr' => [
+                                                     'placeholder' => 'Tapez votre login ...',
+                                                     'class'=>'form-control'
+                                              ],
+                                        'required' => true,
+                    ])
+            ->add('password',PasswordType::class,[
+                                        'label' => 'Mot de passe : ',
+                                        'attr' => [
+                                                    'placeholder' => 'Tapez votre mot de passe ...',
+                                                    'class'=>'form-control'
+                                        ],
+                                        'required' => true,
+                    ])
+            ->add('lastname',TextType::class,[
+                                        'label' => 'Nom : ',
+                                        'attr' => [
+                                                    'placeholder' => 'Tapez votre Nom ...',
+                                                    'class'=>'form-control'
+                                        ],
+                                        'required' => true,
+                ])
+            ->add('firstname',TextType::class,[
+                                        'label' => 'Prénom : ',
+                                        'attr' => [
+                                                    'placeholder' => 'Tapez votre Prénom ...',
+                                                    'class'=>'form-control'
+                                        ],
+                                        'required' => true,
+                ])
+            ->add('email',EmailType::class,[
+                                            'label' => 'Nom : ',
+                                            'attr' => [
+                                                    'placeholder' => 'Tapez votre Email ...',
+                                                    'class'=>'form-control'
+                                                ],
+                                            'required' => true,
+            ])
         ;
     }
 
